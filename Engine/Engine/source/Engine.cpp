015aa8567a4d9ebdd3730af0f9c7ea8e1ee30dca
rogelio-pb <rogelio.pe.2899@gmail.com>
2026-09-13 13:48:33 -0600

    Add DirectX 11 triangle renderer
    This commit adds a working DirectX 11 engine initialization path, shader compilation, vertex buffer setup, and rendering of a colored triangle. It also adds a Win32 sandbox window loop to host the renderer, includes the Triangle.hlsl shader asset, and updates the Visual Studio project files so the shader is treated as a resource instead of a compiled FX target.

--- Engine/Engine/source/Engine.cpp
+++ Engine/Engine/source/Engine.cpp
@@ -110,6 +110,375 @@ Engine::Implementation
             SafeRelease(*shaderBlod);
             return false;
         }
-        return false;
+        return true;
+    }
+
+    void ReleaseResources() noexcept
+    {
+        if (context)
+        {
+            context->ClearState();
+            context->Flush();
+    }
+        SafeRelease(vertexBuffer);
+        SafeRelease(inputLayout);
+        SafeRelease(pixelShader);
+        SafeRelease(vertexShader);
+        SafeRelease(renderTarget);
+        SafeRelease(swapChain);
+        SafeRelease(context);
+        SafeRelease(device);
+
+        window = nullptr;
+        width = 0;
+        height = 0;
     }
 };
+
+Engine::Engine() noexcept
+    :m_implementation(
+        new (std::nothrow) Implementation{}
+    )
+{
+}
+
+Engine::~Engine() noexcept
+{
+    Shutdown();
+
+    delete m_implementation;
+    m_implementation = nullptr;
+}
+
+bool Engine::Initialize(
+    void* nativeWindow,
+    std::uint32_t width,
+    std::uint32_t height
+
+) noexcept
+{
+    if (!m_implementation ||
+        !nativeWindow ||
+        width == 0 ||
+        height == 0)
+    {
+        return false;
+    }
+
+	Implementation& engine = *m_implementation;
+
+    engine.ReleaseResources();
+
+    engine.window = static_cast<HWND>(nativeWindow);
+    engine.width = width;
+    engine.height = height;
+    DXGI_SWAP_CHAIN_DESC swapChainDescription{};
+
+    swapChainDescription.BufferCount = 2;
+    swapChainDescription.BufferDesc.Width = engine.width;
+    swapChainDescription.BufferDesc.Height = engine.height;
+    swapChainDescription.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDescription.BufferDesc.RefreshRate.Numerator = 60;
+    swapChainDescription.BufferDesc.RefreshRate.Denominator = 1;
+    swapChainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDescription.OutputWindow = engine.window;
+    swapChainDescription.SampleDesc.Count = 1;
+    swapChainDescription.SampleDesc.Quality = 0;
+	swapChainDescription.Windowed = TRUE;
+    swapChainDescription.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    constexpr D3D_FEATURE_LEVEL featureLevels[]{
+        D3D_FEATURE_LEVEL_11_0
+    };
+    
+	D3D_FEATURE_LEVEL selectedFeatureLevel{};
+
+    HRESULT result = D3D11CreateDeviceAndSwapChain(
+        nullptr,
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        0,
+        featureLevels,
+        ARRAYSIZE(featureLevels),
+        D3D11_SDK_VERSION,
+        &swapChainDescription,
+        &engine.swapChain,
+        &engine.device,
+        &selectedFeatureLevel,
+        &engine.context
+    );
+
+    if (FAILED(result))
+    {
+        SafeRelease(engine.swapChain);
+        SafeRelease(engine.context);
+        SafeRelease(engine.device);
+
+        result = D3D11CreateDeviceAndSwapChain(
+            nullptr,
+            D3D_DRIVER_TYPE_WARP,
+            nullptr,
+            0,
+            featureLevels,
+            ARRAYSIZE(featureLevels),
+            D3D11_SDK_VERSION,
+            &swapChainDescription,
+            &engine.swapChain,
+            &engine.device,
+            &selectedFeatureLevel,
+            &engine.context
+        );
+    }
+    if (FAILED(result))
+    {
+        engine.ReleaseResources();
+        return false;
+    }
+	ID3D11Texture2D* backBuffer = nullptr;
+    result = engine.swapChain->GetBuffer(
+        0,
+        __uuidof(ID3D11Texture2D),
+        reinterpret_cast<void**>(&backBuffer)
+    );
+
+    if(FAILED(result))
+    {
+        engine.ReleaseResources();
+        return false;
+    }
+    result = engine.device->CreateRenderTargetView(
+        backBuffer,
+        nullptr,
+        &engine.renderTarget
+    );
+    SafeRelease(backBuffer);
+
+    if (FAILED(result))
+    {
+        engine.ReleaseResources();
+        return false;
+    }
+    D3D11_VIEWPORT viewport{};
+
+    viewport.TopLeftX = 0.0f;
+    viewport.TopLeftY = 0.0f;
+
+    viewport.Width =
+        static_cast<float>(engine.width);
+
+    viewport.Height =
+        static_cast<float>(engine.height);
+    viewport.MinDepth = 0.0f;
+    viewport.MaxDepth = 1.0f;
+
+    engine.context->RSSetViewports(
+        1,
+        &viewport
+    );
+
+    ID3DBlob* vertexShaderBlob = nullptr;
+    ID3DBlob* pixelShaderBlob = nullptr;
+
+    if (!Implementation::CompileShader(
+        L"shaders\\Triangle.hlsl",
+        "VSMain",
+        "vs_5_0",
+        &vertexShaderBlob))
+    {
+        engine.ReleaseResources();
+        return false;
+    }
+
+    if (!Implementation::CompileShader(
+        L"shaders\\Triangle.hlsl",
+        "PSMain",
+        "ps_5_0",
+        &pixelShaderBlob))
+    {
+        SafeRelease(vertexShaderBlob);
+        engine.ReleaseResources();
+        return false;
+    }
+    result = engine.device->CreateVertexShader(
+        vertexShaderBlob->GetBufferPointer(),
+        vertexShaderBlob->GetBufferSize(),
+        nullptr,
+        &engine.vertexShader
+    );
+
+    if (FAILED(result))
+    {
+        SafeRelease(pixelShaderBlob);
+        SafeRelease(vertexShaderBlob);
+		engine.ReleaseResources();
+        return false;
+    }
+    result = engine.device->CreatePixelShader(
+        pixelShaderBlob->GetBufferPointer(),
+        pixelShaderBlob->GetBufferSize(),
+        nullptr,
+        &engine.pixelShader
+    );
+    
+    if (FAILED(result))
+    {
+		SafeRelease(pixelShaderBlob);
+		SafeRelease(vertexShaderBlob);
+        engine.ReleaseResources();
+        return false;
+    }
+
+    constexpr D3D11_INPUT_ELEMENT_DESC inputElements[]{
+        {"Position", 0, DXGI_FORMAT_R32G32B32_FLOAT,0, static_cast<UINT>(offsetof(Implementation::Vertex, position)), D3D11_INPUT_PER_VERTEX_DATA, 0},
+        {"Color", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, static_cast<UINT>(offsetof(Implementation::Vertex, color)), D3D11_INPUT_PER_VERTEX_DATA, 0}
+    };
+
+    result = engine.device->CreateInputLayout(
+        inputElements,
+        ARRAYSIZE(inputElements),
+        vertexShaderBlob->GetBufferPointer(),
+        vertexShaderBlob->GetBufferSize(),
+        &engine.inputLayout
+    );
+
+    SafeRelease(pixelShaderBlob);
+    SafeRelease(vertexShaderBlob);
+
+	if (FAILED(result))
+	{
+		engine.ReleaseResources();
+		return false;
+	}
+
+    constexpr Implementation::Vertex vertices[]
+    {
+        {
+            {0.0f, 0.6f, 0.0f},
+            {1.0f, 0.0f, 0.0f, 1.0f }
+        },
+        {
+            {0.6f, -0.6f, 0.0f },
+            {0.0f, 1.0f, 0.0f, 1.0f }
+        },
+        {
+            {-0.6f, -0.6f, 0.0f },
+            {0.0f, 0.3f, 1.0f, 1.0f}
+        }
+
+    };
+
+    D3D11_BUFFER_DESC vertexBufferDescription{};
+    
+    vertexBufferDescription.ByteWidth =
+        static_cast<UINT>(sizeof(vertices));
+
+    vertexBufferDescription.Usage =
+        D3D11_USAGE_IMMUTABLE;
+
+    vertexBufferDescription.BindFlags =
+        D3D11_BIND_VERTEX_BUFFER;
+
+    vertexBufferDescription.CPUAccessFlags = 0;
+    vertexBufferDescription.MiscFlags = 0;
+    vertexBufferDescription.StructureByteStride = 0;
+
+    D3D11_SUBRESOURCE_DATA initialVertexData{};
+    initialVertexData.pSysMem = vertices;
+
+    result = engine.device->CreateBuffer(
+        &vertexBufferDescription,
+        &initialVertexData,
+        &engine.vertexBuffer
+    );
+
+    if (FAILED(result))
+    {
+        engine.ReleaseResources();
+        return false;
+    }
+
+    return true;
+}
+
+void Engine::Render() noexcept
+{
+    if (!m_implementation)
+        return;
+
+    Implementation& engine = *m_implementation;
+
+    if (!engine.context ||
+        !engine.swapChain ||
+        !engine.renderTarget ||
+        !engine.vertexBuffer ||
+        !engine.inputLayout ||
+        !engine.vertexShader ||
+        !engine.pixelShader)
+    {
+        return;
+    }
+
+    constexpr float clearColor[]
+    {
+        0.03f,
+        0.04f,
+        0.08f,
+        1.0f
+    };
+
+    engine.context->OMSetRenderTargets(
+        1,
+        &engine.renderTarget,
+        nullptr
+    );
+
+    engine.context->ClearRenderTargetView(
+        engine.renderTarget,
+        clearColor
+    );
+
+    constexpr UINT stride =
+        sizeof(Implementation::Vertex);
+
+    constexpr UINT offset = 0;
+
+    engine.context->IASetVertexBuffers(
+        0,
+        1,
+        &engine.vertexBuffer,
+        &stride,
+        &offset
+    );
+
+    engine.context->IASetInputLayout(
+        engine.inputLayout
+    );
+
+    engine.context->IASetPrimitiveTopology(
+        D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST
+    );
+
+    engine.context->VSSetShader(
+        engine.vertexShader,
+        nullptr,
+        0
+    );
+
+    engine.context->PSSetShader(
+        engine.pixelShader,
+        nullptr,
+        0
+    );
+
+    engine.context->Draw(3, 0);
+    engine.swapChain->Present(1, 0);
+}
+
+void Engine::Shutdown() noexcept
+{
+    if (m_implementation)
+        m_implementation->ReleaseResources();
+}
+
+
+
--- Engine/SandBox/source/main.cpp
+++ Engine/SandBox/source/main.cpp
@@ -1,41 +1,210 @@
+#define WIN_LEAN_AND_MEAN
+#define NOMINMAX
 #include <Windows.h>
-
 #include <Engine/Engine.h>
 
-#include <string>
+constexpr wchar_t WINDOW_CLASS_NAME[] =
+L"Spider Engine";
+
+LRESULT CALLBACK
+WindowProcedure(
+    HWND window,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+)
+
+{
+    switch (message)
+    {
+    case WM_CLOSE:
+        DestroyWindow(window);
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_ERASEBKGND:
+        return 1;
+
+    default:
+        return DefWindowProcW(
+            window,
+            message,
+            wParam,
+            lParam
+        );
+    }
+}
+
 
 int WINAPI wWinMain(
-    HINSTANCE,
-    HINSTANCE,
-    PWSTR,
-    int
-)
-{
-    const int testResult = Engine_RunSmokeTest();
+    HINSTANCE previousInstance,
+    HINSTANCE instance,
+    PWSTR commandLine,
+    int showCommand){
+    UNREFERENCED_PARAMETER(previousInstance);
+    UNREFERENCED_PARAMETER(commandLine);
 
-    std::wstring message = L"Engine cargado: ";
-    message += Engine_GetName();
+    constexpr UINT CLIENT_WIDTH = 1280;
+    constexpr UINT CLIENT_HEIGHT = 720;
 
-    if (testResult != 0)
-    {
-        message += L"\n\nDirectX 11: OK";
-        message += L"\nDirectXTK: OK";
-        message += L"\nEngine.dll: OK";
-        message += L"\nEngine.lib: OK";
-    }
-    else
-    {
-        message += L"\n\nError durante la validacion.";
-    }
-
-    MessageBoxW(
+    WNDCLASSEXW windowClass{};
+    windowClass.cbSize = sizeof(WNDCLASSEXW);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = WindowProcedure;
+    windowClass.hInstance = instance;
+    windowClass.hCursor = LoadCursorW(
         nullptr,
-        message.c_str(),
-        L"Sandbox - Smoke Test",
-        testResult != 0
-        ? MB_OK | MB_ICONINFORMATION
-        : MB_OK | MB_ICONERROR
+        IDC_ARROW
+    );
+    windowClass.lpszClassName = WINDOW_CLASS_NAME;
+
+    if (!RegisterClassExW(&windowClass))
+    {
+        MessageBoxW(
+            nullptr,
+            L"No se pudo registrar la clase de ventana.",
+            L"Sandbox Error",
+            MB_OK | MB_ICONERROR
+        );
+        return 1;
+    }
+
+    //ventana fija mientras no implementes ResizzeBuffers.
+    constexpr DWORD windowStyle =
+        WS_OVERLAPPED |
+        WS_CAPTION |
+        WS_SYSMENU |
+        WS_MINIMIZEBOX;
+
+    RECT windowRectangle
+    {
+        0,
+        0,
+        static_cast<LONG>(CLIENT_WIDTH),
+        static_cast<LONG>(CLIENT_HEIGHT)
+    };
+
+    if (!AdjustWindowRect(
+        &windowRectangle,
+        windowStyle,
+        FALSE))
+    {
+        UnregisterClassW(
+            WINDOW_CLASS_NAME,
+            instance
+        );
+        return 1;
+    }
+
+    const int windowWidth =
+        windowRectangle.right -
+        windowRectangle.left;
+
+    const int windowHeight =
+        windowRectangle.bottom -
+        windowRectangle.top;
+
+    HWND window = CreateWindowExW(
+        0,
+        WINDOW_CLASS_NAME,
+        L"Sandbox - DirectX 11",
+        windowStyle,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        windowWidth,
+        windowHeight,
+        nullptr,
+        nullptr,
+        instance,
+        nullptr
     );
 
-    return testResult != 0 ? 0 : 1;
-}
+    if (!window)
+    {
+        MessageBox(
+            nullptr,
+            L"No se pudo crar la ventana. ",
+            L"Sandbox Error",
+            MB_OK | MB_ICONERROR
+        );
+
+        UnregisterClassW(
+            WINDOW_CLASS_NAME,
+            instance
+        );
+
+        return 1; 
+    }
+   
+    Engine engine;
+    if (!engine.Initialize(window, CLIENT_WIDTH, CLIENT_HEIGHT))
+    {
+        MessageBoxW(
+            window,
+            L"No se pudo inicializar el Engine. \n\n"
+            L"Verifica que exista:\n"
+            L"shaders\\Triangle.hlsl\n\n"
+            L"Reisa tambien la ventana Output. ",
+            L"Engine Error",
+            MB_OK | MB_ICONERROR
+        );
+
+        DestroyWindow(window);
+
+            UnregisterClassW(
+                WINDOW_CLASS_NAME,
+                instance
+            );
+
+            return 1;
+    }
+
+    ShowWindow(window, showCommand);
+    UpdateWindow(window);
+
+    MSG message{};
+    bool running = true;
+
+
+    while (running)
+    {
+        while (PeekMessageW(
+            &message,
+            nullptr,
+            0,
+            0,
+            PM_REMOVE))
+        {
+            if (message.message == WM_QUIT)
+            {
+                running = false;
+                break;
+            }
+            
+            TranslateMessage(&message);
+            DispatchMessageW(&message);
+        }
+
+        if (!running)
+            break;
+
+        if (IsIconic(window))
+        {
+            WaitMessage();
+            continue;
+        }
+
+        engine.Render();
+    }
+
+    engine.Shutdown();
+
+    UnregisterClass(
+        WINDOW_CLASS_NAME,
+        instance
+    );
+    return 0;
+
+   }
